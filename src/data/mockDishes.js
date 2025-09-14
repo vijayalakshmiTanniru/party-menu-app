dd83c9ca41f8c3357b93fadd66e0afc8c9f80b8d
vijayalakshmiTanniru <vijayalakshmitanniru98@gmail.com>
2025-09-14 21:13:35 +0530

    Create mockDishes.js

---
+++ src/data/mockDishes.js
@@ -0,0 +1,40 @@
+const DISHES = [
+  {
+    id: 1,
+    name: "Spring Rolls",
+    type: "Starter",
+    isVeg: true,
+    description: "Crispy rolls stuffed with veggies",
+    image: "https://via.placeholder.com/150",
+    ingredients: ["Cabbage", "Carrot", "Onion", "Wrapper"]
+  },
+  {
+    id: 2,
+    name: "Chicken Tikka",
+    type: "Starter",
+    isVeg: false,
+    description: "Grilled chicken with spices",
+    image: "https://via.placeholder.com/150",
+    ingredients: ["Chicken", "Yogurt", "Spices"]
+  },
+  {
+    id: 3,
+    name: "Paneer Butter Masala",
+    type: "Main Course",
+    isVeg: true,
+    description: "Paneer cubes in rich tomato gravy",
+    image: "https://via.placeholder.com/150",
+    ingredients: ["Paneer", "Tomato", "Cream", "Butter"]
+  },
+  {
+    id: 4,
+    name: "Gulab Jamun",
+    type: "Dessert",
+    isVeg: true,
+    description: "Sweet milk-solid dumplings in syrup",
+    image: "https://via.placeholder.com/150",
+    ingredients: ["Khoya", "Sugar Syrup", "Cardamom"]
+  }
+];
+
+export default DISHES;
